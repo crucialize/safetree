003da0fea34c7e0660d8d7a0ecfeaa0444f7e27a
crucialize <crucialize@126.com>
2018-05-26 20:56:45 +0800

    Rewrite: request parsing.

--- safetree/SafetreeClient.cs
+++ safetree/SafetreeClient.cs
@@ -47,13 +47,48 @@ namespace safetree
 	{
 		public List<RestRequest> ReqList = new List<RestRequest>();
 
+		public void AddRequest(RestRequest req)
+		{
+			ReqList.Add(req);
+			Console.WriteLine($"Request detected: {req.Method.ToString()} {req.Resource}");
+		}
+
 		public static SafetreeTask ParseCourse(string HAR)
 		{
+			string[] Containing =
+			{
+				"_method=SkillCheckName",
+				"_method=TemplateIn2",
+				"SubmitTest",
+				"Step=1",
+
+			};
+
 			var har = HAR.FromJSON<HARFile>();
 			var reqs = from p in har.log.entries select p.request;
 			
 			var t = new SafetreeTask();
 
+			foreach(var req in reqs)
+			{
+				foreach(var con in Containing)
+				{
+					if (req.url.Contains(con))
+					{
+						t.AddRequest(req.ToRequest());
+
+
+						if(con== "Step=1")
+						{
+							var clone = req;
+							clone.url=clone.url.Replace("Step=1", "Step=2");
+							t.AddRequest(clone.ToRequest());
+						}
+						break;
+					}
+				}
+			}
+			/*
 			try
 			{
 				var H_CheckCourse = (from p in reqs where p.method == "POST" && p.url.Contains("_method=SkillCheckName") select p).ToArray()[0];
@@ -86,18 +121,11 @@ namespace safetree
 				t.ReqList.Add(H_step1.ToRequest());
 			}
 			catch { }
-/*
-			try
-			{
-				//FinishWork
-				var H_Finish = (from p in reqs where p.method == "GET" && p.url.Contains("FinishWork") select p).ToArray()[0];
-				t.ReqList.Add(H_Finish.ToRequest());
-			}
-			catch { }
-*/
+			*/
 
 			return t;
 		}
+
 
 	}
 
